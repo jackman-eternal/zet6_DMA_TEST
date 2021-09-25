0bf978d3049025022bbfbec71e4dd7bfdaaff1bd
jackman-eternal <a7790@365vip.pro>
2021-09-25 20:00:58 +0800

    提交修改

--- user/main.c
+++ user/main.c
@@ -37,28 +37,28 @@ int main(void)
 
 	while(1)
 	{
-//		ADC_Value_Temp1 = (float)ADC_Value[0]*3.3/4096;
-//		ADC_Value_Temp2 = (float)ADC_Value[1]*3.3/4096;
-       delay_ms(2000);
-	   
+	   ADC_Value_Temp1 = (float)ADC_Value[0]*3.3/4096;
+	   ADC_Value_Temp2 = (float)ADC_Value[1]*3.3/4096;
+       delay_ms(2000); 
   //   printf("Value1= %f,Value2= %f\r\n",ADC_Value_Temp1,ADC_Value_Temp2)  ;
-  //   printf("{\"zhuo\":%f, \"temperature\":3,\"NH3\":4,\"O2\":4,\"pH\":1.0}",ADC_Value_Temp);		
+     printf("{\"zhuo\":2.0, \"temperature\":3,\"NH3\":4,\"O2\":4,\"pH\":1.0}");		//ADC_Value_Temp1
 	}
-	
-	
-//	while(1)
-//	{
-//		OLED_Fill(0xFF);//È«ÆÁµãÁÁ
-//        delay_ms(2000); 
-////		for(i=0;i<5;i++)
-////		{
-////			OLED_ShowCN(22+i*16,0,i);//²âÊÔÏÔÊ¾ÖÐÎÄ
-////		}
-////        delay_ms(2000); 
-////		OLED_ShowStr(0,3,"HelTec Automation",1);//²âÊÔ6*8×Ö·û
-////		OLED_ShowStr(0,4,"Hello Tech",2);				//²âÊÔ8*16×Ö·û
-////		delay_ms(2000); 
-////		OLED_CLS();//ÇåÆÁ
-//	}
 }
 
+	
+/*	
+	while(1)
+	{
+		OLED_Fill(0xFF);//È«ÆÁµãÁÁ
+        delay_ms(2000); 
+		for(i=0;i<5;i++)
+		{
+			OLED_ShowCN(22+i*16,0,i);//²âÊÔÏÔÊ¾ÖÐÎÄ
+		}
+        delay_ms(2000); 
+		OLED_ShowStr(0,3,"HelTec Automation",1);//²âÊÔ6*8×Ö·û
+		OLED_ShowStr(0,4,"Hello Tech",2);				//²âÊÔ8*16×Ö·û
+		delay_ms(2000); 
+		OLED_CLS();//ÇåÆÁ
+	}
+*/
