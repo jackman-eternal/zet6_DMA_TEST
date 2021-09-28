1884e1d27163796dd0f38ad430e3e912af373867
jackman-eternal <a7790@365vip.pro>
2021-09-28 17:02:26 +0800

    提交

--- user/main.c
+++ user/main.c
@@ -21,7 +21,7 @@ int main(void)
 	 ADC_TU_Temp2 = (float)ADC_Value[1]*3.3/4096;	
      delay_ms(1000);
 	 delay_ms(1000); 
-     printf("ADC_PH_Temp1 = %f \r\n",ADC_PH_Temp1);  
+     printf("ADC_PH_Temp1 = %f  \r\n",ADC_PH_Temp1);  
      printf("ADC_TU_Temp2 = %f  \r\n",ADC_TU_Temp2); 		
   // printf("Value1= %f,Value2= %f\r\n",ADC_Value_Temp1,ADC_Value_Temp2)  ;
   // printf("{\"zhuo\":2.0,\"temperature\":3,\"NH3\":4,\"O2\":4,\"pH\":%f}",ADC_PH_Temp1);		//ADC_Value_Temp1
